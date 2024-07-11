496bcd297586924914936cd15200fd9e9f827698
Adriele <adriele.dejprimo@gmail.com>
2024-07-11 20:51:27 -0300

    Implementing a txt file with open ports

--- index.py
+++ index.py
@@ -3,14 +3,21 @@ from portScanner import scan_port
 
 def main():
     ip = input("Enter the IP address for port scanning: ")
+    open_ports = []
+    
     try:
         for port in range(1, 1025):
-            scan_port(ip, port)
+            if scan_port(ip, port):
+                print(f"Port {port}: Open")
+                open_ports.append(port)
     except KeyboardInterrupt:
         print("\nScan interrupted by the user")
     except socket.gaierror:
         print("Error resolving host")
     except socket.error:
         print("Unable to connect to the server")
+    with open("open_ports.txt", "w") as file:
+        for port in open_ports:
+            file.write(f"Port{port}: Open\n")
 if __name__ == "__main__":
     main()
--- portScanner.py
+++ portScanner.py
@@ -4,20 +4,13 @@ def scan_port(ip, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
-
         result = sock.connect_ex((ip, port))
-        if result == 0:
-            print(f"Port {port}: Open")
-        else:
-            print(f"Port {port}: Closed")
-
         sock.close()
+        if result == 0:
+            return True
+        else:
+            return False
+
     except socket.error:
-        print(f"Error connecting to port{port}")
+        return False
 
-if __name__ == "__main__":
-    ip = "ip"
-    ports =  [80, 443, 22]
-
-    for port in ports:
-        scan_port(ip, port)
