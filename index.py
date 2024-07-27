e7a377366d43ac0a63120a5026b2dc4e6cacd487
Adriele <adriele.dejprimo@gmail.com>
2024-07-27 20:14:37 -0300

    Added the service to file.write

--- index.py
+++ index.py
@@ -7,17 +7,19 @@ def main():
     
     try:
         for port in range(1, 1025):
-            if scan_port(ip, port):
-                print(f"Port {port}: Open")
-                open_ports.append(port)
+            is_open, service = scan_port(ip, port)
+            if is_open:
+                print(f"Port {port} ({service}): Open")
+                open_ports.append((port, service))
     except KeyboardInterrupt:
         print("\nScan interrupted by the user")
     except socket.gaierror:
         print("Error resolving host")
     except socket.error:
         print("Unable to connect to the server")
+
     with open("open_ports.txt", "w") as file:
         for port in open_ports:
-            file.write(f"Port{port}: Open\n")
+            file.write(f"Port{port} ({service}) : Open\n")
 if __name__ == "__main__":
     main()
