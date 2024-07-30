7aebb6e8eb11e0b0072254be1a73292ff18a6f04
Adriele <adriele.dejprimo@gmail.com>
2024-07-30 20:12:46 -0300

    Using the  threading in the index.py

--- index.py
+++ index.py
@@ -1,16 +1,19 @@
+import threading
 import socket
-from portScanner import scan_port
+from threadingScan import thread_scan
 
 def main():
     ip = input("Enter the IP address for port scanning: ")
     open_ports = []
-    
+    threads = []
     try:
         for port in range(1, 1025):
-            is_open, service = scan_port(ip, port)
-            if is_open:
-                print(f"Port {port} ({service}): Open")
-                open_ports.append((port, service))
+            thread = threading.Thread(target=thread_scan, args=(ip, port, open_ports))
+            threads.append(thread)
+            thread.start()
+
+            for thread in threads:
+                thread.join()
     except KeyboardInterrupt:
         print("\nScan interrupted by the user")
     except socket.gaierror:
@@ -19,7 +22,7 @@ def main():
         print("Unable to connect to the server")
 
     with open("open_ports.txt", "w") as file:
-        for port in open_ports:
+        for port, service  in open_ports:
             file.write(f"Port{port} {service} : Open\n")
 if __name__ == "__main__":
     main()
